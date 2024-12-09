5e9f04af2fa8d354bdfa1677f250d7b278239eb0
Mina Dobrijevic <minadobrijevic@Minas-MacBook-Air.local>
2024-12-09 15:27:41 -0500

    mod model

---
+++ models/Moderator.js
@@ -0,0 +1,23 @@
+//Moderators(userId, clubId), pair is unique, single attributes can be repeated
+
+const mongoose = require("mongoose");
+
+const moderatorSchema = new mongoose.Schema(
+    {
+        userID: {
+            type: Number,
+            required: true
+        },
+        clubID: {
+            type: Number, 
+            required: true
+        },
+    }, 
+
+    {
+    timestamps: true,
+    collection: "Moderators",
+    }
+);
+
+module.exports = mongoose.model("Moderator", moderatorSchema);
